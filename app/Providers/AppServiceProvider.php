6ecad0ecb4ec7be823404594586829ca80bd1d7e
vuxjqk <vuxjqk@gmail.com>
2025-06-21 11:58:05 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Gate::define('is-admin', function ($user) {
+            return $user->role === 'admin';
+        });
+
+        Gate::define('is-doctor', function ($user) {
+            return $user->role === 'doctor';
+        });
+
+        Gate::define('is-patient', function ($user) {
+            return $user->role === 'patient';
+        });
+    }
+}
